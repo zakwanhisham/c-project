f292c816875fca3caee7c792980e9b349a044ce6
zakwan hisham <hishamzakwan@gmail.com>
2023-07-12 12:26:14 +0800

    Adding build script that can be used to easily build the program

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -xe
+
+CFLAGS="-O3 -Wall -Wextra -ggdb -I./thirdparty/ -I. $(pkg-config --cflags raylib)"
+LIBS="-lm $(pkg-config --libs raylib) -lglfw -ldl -lpthread"
+
+mkdir -p out
+
+# clang -o out/calculator calculator.c -lm
+clang $CFLAGS -o out/student_management_system student_management_system.c $LIBS
