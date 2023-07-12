832f0e60c9c78476306d0629af112bbe90d904ad
zakwan hisham <hishamzakwan@gmail.com>
2023-07-12 16:09:12 +0800

    Add input gesture detection

--- build.sh
+++ build.sh
@@ -8,4 +8,5 @@ LIBS="-lm $(pkg-config --libs raylib) -lglfw -ldl -lpthread"
 mkdir -p out
 
 # clang -o out/calculator calculator.c -lm
-clang $CFLAGS -o out/student_management_system student_management_system.c $LIBS
+# clang $CFLAGS -o out/student_management_system student_management_system.c $LIBS
+clang $CFLAGS -o out/input_gesture input_gesture_detection.c $LIBS
